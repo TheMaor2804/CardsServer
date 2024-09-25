4ed5c84f978e2dbf69bd0cd3c562c3c98f2f7df7
TheMaor2804 <mhgameing@Gmail.com>
2024-09-25 15:31:14 +0300

    Refactor cors.js to remove unused origin and add "https://cardswebsite.onrender.com" to allowed origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const corsMiddleware = cors({
   origin: [
     "http://127.0.0.1:8181",
-    "www.tzachCards.co.il",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
     "https://cardswebsite.onrender.com",
